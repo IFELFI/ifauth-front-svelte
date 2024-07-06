81860e4a3a2772a78d7952c50bb0e4a4bf406378
sauce-git <sauce@mandacode.com>
2024-07-06 21:34:11 +0900

    chore: Remove unused code and optimize logout load function

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -4,14 +4,13 @@ import type { PageServerLoad } from './$types';
 import { PUBLIC_HOME_URL } from '$env/static/public';
 import { session } from '$lib/api/urls';
 
-export const load: PageServerLoad = async ({ fetch, cookies }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
 	const logoutApi = session.destroy;
 	await fetch(logoutApi.url, {
 		method: logoutApi.method
 	});
 
 	isValid.set(false);
-  cookies.set('SID', '', { path: '/', maxAge: 0 });
 
 	redirect(302, PUBLIC_HOME_URL || '/');
 };
