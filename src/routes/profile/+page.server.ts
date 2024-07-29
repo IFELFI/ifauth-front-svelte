d8bd77e13322fba057ed991e6583abb4ce951f71
sauce-git <sauce@mandacode.com>
2024-07-29 09:34:27 +0900

    chore: Update profile page server load function

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -3,19 +3,21 @@ import type { Profile } from '$types/data';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { PUBLIC_HOME_URL } from '$env/static/public';
+import path from 'path';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const profileApi = member.profile.get;
 	const response = await fetch(profileApi.url, {
 		method: profileApi.method
 	});
+	
 	if (response.status === 200) {
 		const profile = (await response.json()).data as Profile;
 		return {
 			profile
 		};
-	} else if (response.status === 401) {
-		redirect(302, `${PUBLIC_HOME_URL}/signin`);
+	} else if (response.status === 401 || response.status === 404) {
+		redirect(302, path.join(PUBLIC_HOME_URL, 'signin'));
 	}
 	return {
 		error: 'Failed to load profile'
